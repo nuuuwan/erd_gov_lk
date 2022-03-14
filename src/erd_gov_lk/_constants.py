380cae99919ada11d07afe877f6ebd871d0dc97c
nuuuwan <nuuuwan@gmail.com>
2022-03-14 19:42:37 +0530

    Split code

--- src/erd_gov_lk/_constants.py
+++ src/erd_gov_lk/_constants.py
@@ -1,4 +1,36 @@
 """Constants."""
+from urllib import parse
 
 CACHE_NAME = 'erd_gov_lk'
 CACHE_TIMEOUT = 3600
+
+URL_ERD = 'http://www.erd.gov.lk/index.php'
+
+URL_INDEX = URL_ERD + '?' + parse.urlencode({
+    'option': 'com_rmobilization',
+    'view': 'resource_project',
+    'category_id': '1',
+    'sub_category_id': '1',
+    'searchby': 'donor_type',
+    'Itemid': '321',
+    'lang': 'en',
+})
+
+DONOR_LIST_FILE = '/tmp/erd_gov_lk.donors.tsv'
+PROJECT_LIST_FILE = '/tmp/erd_gov_lk.projects.tsv'
+
+CURRENCY_TO_TEXT_LIST = {
+    'EUR': ['Euro'],
+    'GBP': [],
+    'JPY': ['Yen'],
+    'KWD': ['KD'],
+    'USD': ['US$'],
+}
+
+EXCHANGE_RATE_TO_USD = {
+    'EUR': 1.09,
+    'GBP': 1.30,
+    'JPY': 0.0085,
+    'KWD': 3.29,
+    'USD': 1,
+}
---
+++ src/erd_gov_lk/parse_helpers.py
@@ -0,0 +1,98 @@
+
+from utils import timex
+
+from erd_gov_lk._constants import CURRENCY_TO_TEXT_LIST, EXCHANGE_RATE_TO_USD
+
+
+def parse_amount(raw_amount):
+    amount = None
+    currency = 'USD'
+    amount_mult = 1
+    for word in raw_amount.split(' '):
+        for currency0, text_list in CURRENCY_TO_TEXT_LIST.items():
+            for text in [currency0] + text_list:
+                if text == word:
+                    currency = currency0
+
+        if word.lower() in ['mn', 'million', 'milion']:
+            amount_mult = 1_000_000
+        if word.lower() in ['billion']:
+            amount_mult = 1_000_000_000
+
+        if not amount:
+            try:
+                amount0 = (float)(word.replace(',', ''))
+                amount = amount0
+            except ValueError:
+                pass
+
+    if not amount:
+        return None, None, None
+
+    if amount < 1_000_000 and amount_mult == 1:
+        amount_mult = 1_000_000
+    amount_m = amount * amount_mult / 1_000_000
+    amount_m_usd = amount_m * EXCHANGE_RATE_TO_USD[currency]
+    return amount_m, currency, amount_m_usd
+
+
+def parse_date(raw_date):
+    if not raw_date:
+        return None, None
+
+    ut = None
+    try:
+        ut = timex.parse_time(raw_date, '%Y-%m-%d')
+    except ValueError:
+        pass
+    return raw_date, ut
+
+
+def parse_duration(raw_duration):
+    duration = None
+    duration_unit = None
+    duration_unit_ticks = None
+    duration_s = None
+    if raw_duration:
+        for word in raw_duration.split(' '):
+            try:
+                duration0 = (float)(word.replace(',', ''))
+                duration = duration0
+            except ValueError:
+                pass
+
+            if 'year' in word.lower():
+                duration_unit = 'year'
+                duration_unit_ticks = timex.SECONDS_IN.YEAR
+
+    if duration and duration_unit_ticks:
+        duration_s = duration * duration_unit_ticks
+    return duration, duration_unit, duration_s
+
+
+def parse_project(raw_project):
+    amount, currency, amount_m_usd = parse_amount(raw_project['Amount (USD)'])
+    start_date, start_ut = parse_date(raw_project['Commencement Date'])
+    end_date, end_ut = parse_date(raw_project.get('Completed On'))
+
+    duration, duration_unit, duration_unit_ticks = parse_duration(
+        raw_project.get('Duration'))
+
+    return {
+        'project_name': raw_project['Project Name'],
+        'is_loan': raw_project['Loan / Grant'].lower() == 'loan',
+        'amount_m': amount,
+        'currency': currency,
+        'amount_m_usd': amount_m_usd,
+        'objectives': raw_project['Objective/s'],
+        'main_components': raw_project['Main Component/s'],
+        'start_date': start_date,
+        'start_ut': start_ut,
+        'end_date': end_date,
+        'end_ut': end_ut,
+        'duration': duration,
+        'duration_unit': duration_unit,
+        'duration_unit_ticks': duration_unit_ticks,
+        'implementing_agency': raw_project['Implementing Agency'],
+        'executing_agency': raw_project['Executing Agency'],
+    }
